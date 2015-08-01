915bc621b39450c606bf1f384cb4f206ba6ff9f3
Emily Wu <embrilliant@hotmail.com>
2015-08-01 23:55:08 +0200

    tidied script

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 $(function() {
 
-	var vid = document.getElementById("vid"), //fish
-		vid2 = document.getElementById("vid2"), //bubble
-		vid3 = document.getElementById("bgVid"), //polar bear
+		//Elements
+	var vids = [document.getElementById("vid"), //fish
+				document.getElementById("vid2"), //bubble
+				document.getElementById("bgVid")], //polar bear
 
-		canvas = document.getElementById("canvas"),
-		cDraw = canvas.getContext("2d"),
+		canvases = [document.getElementById("canvas"), 
+					document.getElementById("canvas2")],
 
-		canvasWidth = canvas.width,
-		canvasHeight = canvas.height, //get baseCanvas size
+		mergeCanvases = [document.getElementById("merge"), 
+						document.getElementById("merge2")],
+
+		outputCanvases = [document.getElementById("output"),
+						document.getElementById("output2")],
 		
-		canvas2 = document.getElementById("canvas2"),
-		cDraw2 = canvas2.getContext("2d"),
+		//Canvas renders
+		canvasDraws = [canvases[0].getContext("2d"), 
+						canvases[1].getContext("2d")], 
 
-		merge = document.getElementById("merge"),
-		cMerge = merge.getContext("2d"),
-		merge2 = document.getElementById("merge2"),
-		cMerge2 = merge2.getContext("2d"),
-		
-		outputCanvas = document.getElementById("output"),
-		cOutput = outputCanvas.getContext("2d"),
-		outputCanvas2 = document.getElementById("output2"),
-		cOutput2 = outputCanvas2.getContext("2d"),
+		canvasWidth = canvases[0].width,
+		canvasHeight = canvases[0].height, //get baseCanvas size
+
+		merges = [mergeCanvases[0].getContext("2d"), 
+					mergeCanvases[1].getContext("2d")],
+
+		outputs = [outputCanvases[0].getContext("2d"),
+					outputCanvases[1].getContext("2d")],
 
 		initLayer = true,
 		timeOut;
 
-	function brush(event) { 
+	function brush(draw) { 
 
 		var $this = $(this);
 		
@@ -37,17 +41,17 @@ $(function() {
 					yPos = event.pageY;	// top
 
 				// get canvas position
-				var widthSpace = ($(window).width() - 640) / 2,
-					topSpace = $("#output").position().top;
+				var leftSpace = $(outputCanvases[0]).position().left,
+					topSpace = $(outputCanvases[0]).position().top;
 
-				var brushPosX = xPos - widthSpace,
+				var brushPosX = xPos - leftSpace,
 					brushPosY = yPos - topSpace;
 				// end of get brush position
 
-				cDraw.fillStyle = "rgba(0, 0, 0, 1)";
-				cDraw.beginPath();
-				cDraw.arc(brushPosX, brushPosY, 30, 0, 2 * Math.PI, false);
-				cDraw.fill();
+				draw.fillStyle = "rgba(0, 0, 0, 1)";
+				draw.beginPath();
+				draw.arc(brushPosX, brushPosY, 30, 0, 2 * Math.PI, false);
+				draw.fill();
 			},
 			mouseup: function() {
 				$this.off("mousemove");
@@ -55,103 +59,72 @@ $(function() {
 		});
 	}
 
-	function brush2(event) { 
+  	function manipulation(merge, output) {
 
-		var $this = $(this);
-		
-		$this.on({
-			mousemove: function(event) {
-				var xPos = event.pageX,                   
-					yPos = event.pageY;
+  		for ( var i = 0; i < merges.length; i++ ) {
+  			merges[i].drawImage(vids[i], 0, 0, 640, 360);
+  			merges[i].drawImage(canvases[i], 0, 0, 640, 360);
+  		}
 
-				var widthSpace = ($(window).width() - 640) / 2,
-					topSpace = $("#output2").position().top;
+  		var image = merge.getImageData(0, 0, canvasWidth, canvasHeight),
+			imageData = image.data,
+			length = imageData.length;
 
-				var brushPosX = xPos - widthSpace,
-					brushPosY = yPos - topSpace;
-
-				cDraw2.fillStyle = "rgba(0, 0, 0, 1)";
-				cDraw2.beginPath();
-				cDraw2.arc(brushPosX, brushPosY, 29, 0, 2 * Math.PI, false);
-				cDraw2.fill();
-			},
-			mouseup: function() {
-				$this.off("mousemove");
+		for ( var i = 0; i < length; i += 4 ) {
+			var r = imageData[i],
+				g = imageData[i+1],
+				b = imageData[i+2]; 
+			if (r == 0 && g == 0 && b == 0) {
+	            imageData[i+3] = 0;
 			}
-		});
+		}
+		image.data = imageData;
+		output.putImageData(image, 0, 0, 0, 0, canvasWidth, canvasHeight);
+  	}
+
+  	function whichLayer() {
+
+		if (initLayer) {
+
+			manipulation(merges[0], outputs[0]);
+
+		} else {
+			
+			$("#merge2").css({"display":"none"});
+			manipulation(merges[1], outputs[1]);	
+		}
 	}
 
 	function startToLoop() {
+	    
 	    if (vid.paused || vid.ended) {
 	    	return;
 	    }
-    	manipulation();
+
+    	whichLayer();
+
     	if (requestAnimationFrame) { // "requestAnimationFrame"
         	requestAnimationFrame(startToLoop);
         } else {
     		timeOut = setTimeout(startToLoop, 1000/60);
     	}
+
   	}
 
-  	function manipulation() {
-
-  		cMerge2.drawImage(vid2, 0, 0, 640, 360);
-  		cMerge2.drawImage(canvas2, 0, 0, 640, 360);
-		cMerge.drawImage(vid, 0, 0, 640, 360);
-		cMerge.drawImage(canvas, 0, 0, 640, 360);
-
-		if (initLayer) {
-			
-			var image = cMerge.getImageData(0, 0, canvasWidth, canvasHeight),
-				imageData = image.data,
-				length = imageData.length;
-
-			for ( var i = 0; i < length; i += 4 ) {
-				var r = imageData[i],
-					g = imageData[i+1],
-					b = imageData[i+2]; 
-				if (r == 0 && g == 0 && b == 0) {
-		            imageData[i+3] = 0;
-				}
-			}
-			image.data = imageData;
-			cOutput.putImageData(image, 0, 0, 0, 0, canvasWidth, canvasHeight);
-			
-
-		} else {
-			$("#merge2").css({"display":"none"});
-
-			var image = cMerge2.getImageData(0, 0, canvasWidth, canvasHeight),
-				imageData = image.data,
-				length = imageData.length;
-
-			for ( var i = 0; i < length; i += 4 ) {
-				var r = imageData[i],
-					g = imageData[i+1],
-					b = imageData[i+2]; 
-				if (r == 0 && g == 0 && b == 0) {
-		            imageData[i+3] = 0;
-				}
-			}
-			image.data = imageData;
-			cOutput2.putImageData(image, 0, 0, 0, 0, canvasWidth, canvasHeight);	
-		}
-	}
-
 	$(window).on("mousedown", function() {  
-		vid.play();
-		vid2.play();
+		vids[0].play();
+		vids[1].play();
 		startToLoop();
 		if (initLayer) {
-			brush();
+			brush(canvasDraws[0])
 		} else {
-			brush2();
+			brush(canvasDraws[1])
 		}
 		$("button").css({"visibility":"visible"});
 	});
 
 	$("#switch").on("click", function() {
-		vid3.play();
+		vids[2].play();
 		if (initLayer) {
 			initLayer = false;
 			$(this).text("Switch back");
@@ -161,8 +134,8 @@ $(function() {
 		}
 	});
 
-	vid.play();
-	vid2.play();
+	vids[0].play();
+	vids[1].play();
 	startToLoop();
 
 	vid.addEventListener("ended", function() {
